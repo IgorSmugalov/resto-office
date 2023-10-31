6b50c708993a86b5ad37d16bcb5f8e566dfa225c
Igor Smugalov <smugalovigor@gmail.com>
2023-10-31 13:58:23 +0300

    feat: added protected page HOC

---
+++ src/shared/hoc/with-auth.tsx
@@ -0,0 +1,21 @@
+import { $isAuth } from '@shared/request'
+import { useUnit } from 'effector-react/effector-react.umd'
+import { usePathname, useRouter } from 'next/navigation'
+import { ComponentType, FC, useEffect } from 'react'
+
+const withAuth = <P extends object>(Component: ComponentType<P>): FC<P> => {
+  const Protected: FC<P> = (props) => {
+    const path = usePathname()
+    const router = useRouter()
+    const [isAuth] = useUnit([$isAuth])
+    useEffect(() => {
+      if (!isAuth) router.push(`login?from=${path}`)
+    }, [isAuth, path, router])
+    if (!isAuth) return null
+
+    return <Component {...props} />
+  }
+  return Protected
+}
+
+export default withAuth
