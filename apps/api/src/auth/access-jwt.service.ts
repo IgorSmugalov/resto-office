4b0e1a3c86303cebfc470f9056eacdcf6d0df4ad
Igor Smugalov <smugalovigor@gmail.com>
2023-11-09 21:30:19 +0300

    refactor: access jwt service

--- apps/api/src/auth/access-jwt.service.ts
+++ apps/api/src/auth/access-jwt.service.ts
@@ -1,17 +1,17 @@
+import { AccessJwtClaims, AccessJwtClaimsSchema } from '@libs/schema'
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { User } from '@prisma/client'
-import { KeyLike, SignJWT } from 'jose'
+import { jwtVerify, KeyLike, SignJWT } from 'jose'
 import { IJwtSetConfig, JWT_CONFIG } from '../config'
 import { JwkService } from '../crypto'
-import { AccessJwtClaimsDto } from './dto'
 
 @Injectable()
 export class AccessJwtService {
   private privateJwk: KeyLike
   private publicJwk: KeyLike
   private readonly config =
-    this.configService.get<IJwtSetConfig>(JWT_CONFIG)!.accessJwtConfig
+    this.configService.getOrThrow<IJwtSetConfig>(JWT_CONFIG).accessJwtConfig
 
   constructor(
     private readonly jwkService: JwkService,
@@ -24,21 +24,30 @@ export class AccessJwtService {
   }
 
   public async signJwt(user: User): Promise<string> {
-    const claims = AccessJwtClaimsDto.fromUser(user)
-    const issueTime = Date.now() / 1000
-    return await new SignJWT({ ...claims })
-      .setIssuedAt(issueTime)
-      .setExpirationTime(issueTime + this.config.expiresAfter)
-      .setProtectedHeader({ alg: this.config.algorithm })
+    const issuedAtTime: number = Math.floor(Date.now() / 1000)
+    const expirationTime: number = issuedAtTime + this.config.expiresAfter
+    const payload = AccessJwtClaimsSchema.parse({
+      ...user,
+      iat: issuedAtTime,
+      exp: expirationTime,
+    } as AccessJwtClaims)
+
+    return await new SignJWT(payload)
+      .setProtectedHeader({
+        alg: this.config.algorithm,
+      })
       .sign(this.privateJwk)
   }
 
-  // public async verifyJwt(token: string): Promise<AccessJwtClaimsDto | null> {
-  //   try {
-  //     const { payload } = await jwtVerify(token, this.publicJwk)
-  //     return AccessJwtClaimsDto.fromToken(payload)
-  //   } catch {
-  //     return null
-  //   }
-  // }
+  public async verifyJwt(token: string): Promise<AccessJwtClaims | null> {
+    try {
+      const { payload } = await jwtVerify<AccessJwtClaims>(
+        token,
+        this.publicJwk
+      )
+      return AccessJwtClaimsSchema.parse(payload)
+    } catch {
+      return null
+    }
+  }
 }
